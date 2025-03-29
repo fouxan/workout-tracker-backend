d6f344d10c47c27b5ed405f6c75c5a56a7fe6ff8
fouxan <fouzanfouzan1310@gmail.com>
2025-03-29 12:20:57 +0530

    include routers, update health endpoint

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 from routers import user
 from routers import activity
+from routers import workout_plan
+from routers import workout
 
 app = FastAPI()
 
 
-app.include_router(activity.router)
-# app.include_router(ac.router)
 app.include_router(user.router)
-
+app.include_router(activity.router)
+app.include_router(workout.router)
+app.include_router(workout_plan.router)
 
 
 @app.get("/")
