cc20d43a28accd261bf053a92d64ea70ea932ec6
fouxan <fouzanfouzan1310@gmail.com>
2025-03-29 11:27:06 +0530

    rename files

--- models/otp.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import Column, BigInteger, String, DateTime, Boolean, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime, timedelta
-import secrets
-
-Base = declarative_base()
