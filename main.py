7699b49e6157545a3adf45ceef3065368c4e671b
fouxan <fouzanfouzan1310@gmail.com>
2025-04-04 07:24:15 +0530

    use uvicorn

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import uvicorn
 from fastapi import FastAPI
 from routers import auth
 from routers import activity
 from routers import workout_plan
 from routers import workout
+
 
 app = FastAPI()
 
@@ -16,3 +18,7 @@ app.include_router(workout_plan.router)
 @app.get("/healthcheck")
 async def root():
     return {"message": "Ok!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
